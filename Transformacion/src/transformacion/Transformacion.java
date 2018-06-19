64c0f59b14699b0493363078056d7d73b95f76e2
Gabiita <38547554+Gabiita@users.noreply.github.com>
2018-06-18 23:06:39 -0500

    Add files via upload

---
+++ Transformacion/src/transformacion/Transformacion.java
@@ -0,0 +1,14 @@
+
+package transformacion;
+
+public class Transformacion {
+
+    
+    public static void main(String[] args) {
+        VentanaTransformacion Titulo = new VentanaTransformacion();
+        Titulo.setVisible(true);
+        Titulo.setTitle("Transformacion");
+        Titulo.setLocationRelativeTo(null);
+    }
+    
+}
